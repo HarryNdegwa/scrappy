6865c990a907b9025972329b02ec465fad213620
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-12-12 21:51:03 +0300

    tested get images logic

--- src.py
+++ src.py
@@ -9,4 +9,8 @@ html = page.read().decode("utf-8")
 
 soup = BeautifulSoup(html,"html.parser")
 
-print(soup.get_text())
+# print(soup.get_text())  # prints all text from the html without html tags
+
+# print(soup.find("img")) # returns the first image
+
+# print(soup.find_all("img")) # returns all images in that page
